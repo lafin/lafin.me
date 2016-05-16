37466f4b9a4860d5c0e8ed94622ca793dbdee2a4
Kuvakin Sergey <kuvakin@gmail.com>
2016-05-17 00:00:40 +0300

    :walking:

--- webpack.config.js
+++ webpack.config.js
@@ -3,61 +3,44 @@ var precss = require('precss')
 var webpack = require('webpack')
 var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CompressionPlugin = require('compression-webpack-plugin')
+
+var contentPath = path.join(__dirname, './client')
 
 module.exports = {
-  devtool: 'inline-source-map',
-  context: path.join(__dirname, './client'),
+  context: contentPath,
   entry: {
-    js: './index.js',
-    vendor: []
+    js: './index.js'
   },
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'bundle.js',
   },
   module: {
-    loaders: [
-      {
-        test: /\.jade$/,
-        include: /client/,
-        loader: 'jade-loader'
-      },
-      {
-        test: /\.css$/,
-        include: /client/,
-        loaders: [
-          'style-loader',
-          'css-loader?sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'csso-loader',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=\d\.\d\.\d)?$/,
-        loader: 'url-loader'
-      },
-      {
-        test: /\.css$/,
-        exclude: /client/,
-        loaders: [
-          'style-loader',
-          'css-loader?sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'csso-loader'
-        ]
-      },
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-    ],
+    loaders: [{
+      test: /\.jade$/,
+      include: /client/,
+      loader: 'jade-loader'
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract(
+        'style-loader',
+        'css-loader?sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!csso-loader!postcss-loader')
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=\d\.\d\.\d)?$/,
+      loader: 'url-loader'
+    }, {
+      test: /\.js$/,
+      include: /client/,
+      loader: 'babel-loader'
+    }, ],
   },
   resolve: {
     modulesDirectories: [
       'client',
       'node_modules',
-    ],
-    extensions: ['', '.js']
+    ]
   },
   postcss: [
     precss,
@@ -66,21 +49,35 @@ module.exports = {
     })
   ],
   plugins: [
+    new ExtractTextPlugin('style.css', {
+      allChunks: true
+    }),
     new HtmlWebpackPlugin({
       filename: '../index.html',
       template: 'index.jade'
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false
+      compress: {
+        warnings: false
+      }
     }),
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
     new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      }
     }),
-    new webpack.NoErrorsPlugin()
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|html|css)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
   ],
   devServer: {
-    contentBase: './client',
-    hot: true
+    contentBase: contentPath,
+    compress: true,
+    hot: process.env.NODE_ENV === 'development'
   }
 }
