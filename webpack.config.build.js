61d63b3e34a8a11f1903f0d5e39a0656c443ab4f
Kuvakin Sergey <kuvakin@gmail.com>
2017-09-21 00:02:21 +0300

    go ahead

---
+++ webpack.config.build.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpackConfig = require("./webpack.config");
+
+module.exports = Object.assign(webpackConfig, {
+    devtool: "cheap-module-source-map",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].[chunkhash].js"
+    },
+    plugins: webpackConfig.plugins.concat([
+        new CleanWebpackPlugin(["dist"])
+    ])
+});
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const IS_DEV = (process.env.NODE_ENV === "dev");
+const dirNode = "node_modules";
+const dirApp = path.join(__dirname, "app");
+const dirAssets = path.join(__dirname, "assets");
+
+module.exports = {
+    entry: {
+        // vendor: [],
+        bundle: path.join(dirApp, "index")
+    },
+    resolve: {
+        modules: [
+            dirNode,
+            dirApp,
+            dirAssets
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEV
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "index.jade")
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: "babel-loader",
+            exclude: /(node_modules)/,
+            options: {
+                compact: true
+            }
+        }, {
+            test: /\.css$/,
+            use: [
+                "style-loader",
+                {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: IS_DEV
+                    }
+                }
+            ]
+        }, {
+            test: /\.scss/,
+            use: [
+                "style-loader", {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: IS_DEV
+                    }
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        sourceMap: IS_DEV,
+                        includePaths: [dirAssets]
+                    }
+                }]
+        }, {
+            test: /\.jade$/,
+            loader: "pug-loader"
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf|svg|json)$/,
+            loader: "file-loader",
+            options: {
+                name: "[path][name].[ext]"
+            }
+        }]
+    }
+};
