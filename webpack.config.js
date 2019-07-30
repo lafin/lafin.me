be4f153c1523f99d2485558b01c50ad8352a4ba4
Kuvakin Sergey <kuvakin@gmail.com>
2019-07-30 23:10:57 +0300

    feat(deps): bump feat(text): changed the name translation and typo feat(text): changed link to cv feat(text): changed job title feat(html): fixed dns-prefetch and source

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
